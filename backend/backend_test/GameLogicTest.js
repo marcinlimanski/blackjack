56c14350b92fcf9cc8b4853ec88345fcab109bf5
Marcin Limanski <marcin.limanski@pendo.io>
2024-01-03 14:47:38 -0500

    gamelogic_test: code clean up

--- backend/backend_test/GameLogicTest.js
+++ backend/backend_test/GameLogicTest.js
@@ -1,7 +1,6 @@
 const assert = require('chai').assert;
 const chai = require('chai');
 const GameLogic = require('../utils/gameLogic');
-const nock = require('nock');
 const expect = chai.expect;
 
 describe('GameLogic', function () {
@@ -10,33 +9,22 @@ describe('GameLogic', function () {
     beforeEach(function () {
         // Create a new instance of GameLogic before each test
         game = new GameLogic();
-
-    });
-
-    afterEach(() => {
-        // Clean up nock after each test
-        nock.cleanAll();
     });
 
     it('should initialize a new game state', function () {
         // Assert that the player and dealer have empty cards and a score of 0
         const player = game.getPlayer();
         const dealer = game.getDealer();
-
         assert.deepEqual(player.cards, []);
         assert.equal(player.score, 0);
-
         assert.deepEqual(dealer.cards, []);
         assert.equal(dealer.score, 0);
     });
 
     it('should deal a card', async function () {
-
         // Deal a card and assert that it's a valid card (between 1 and 10)
         await game.getNewDeck();
-
         const card = await game.dealCard();
-
         expect(game.getPlayDeck().length).to.equal(43);
         assert.isNumber(card);
         assert.isAtLeast(card, 1);
@@ -69,7 +57,6 @@ describe('GameLogic', function () {
         // After hit, player & dealer will have a card. Once rest, deck should be empty
         await game.getNewDeck();
         assert.equal(game.getPlayDeck().length, 44);
-
         const card = await game.dealCard();
         assert.equal(game.getPlayDeck().length, 43);
     });
@@ -90,7 +77,6 @@ describe('GameLogic', function () {
         await game.startGame();
         await game.stand();
         let gameStats = game.checkScore();
-
         expect(gameStats.playerStats.cards).to.be.a('array');
         expect(gameStats.playerStats.score).to.be.a('number');
         expect(gameStats.dealerStats.cards).to.be.a('array');
