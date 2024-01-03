a19de09e96885ce23017dd45523a2c196712e104
Marcin Limanski <marcin.limanski@pendo.io>
2024-01-03 14:16:55 -0500

    backend_test: removing the nock dep on test

--- backend/backend_test/GameLogicTest.js
+++ backend/backend_test/GameLogicTest.js
@@ -32,42 +32,24 @@ describe('GameLogic', function () {
 
     it('should deal a card', async function () {
 
-        // Configure nock to mock the API endpoint
-        nock('https://u0xv62mbgj-u0etdf90is-connect.us0-aws.kaleido.io')
-            .get('/instances/0xd6a231bd45de2c6b1d200c2b5778c810e53cd134/shuffleCards')
-            .query(true)
-            .reply(200, { output: ['1', '2', '3', '4', '10', '11'] });
-
         // Deal a card and assert that it's a valid card (between 1 and 10)
         await game.getNewDeck();
 
         const card = await game.dealCard();
 
-        expect(game.getPlayDeck().length).to.equal(5);
+        expect(game.getPlayDeck().length).to.equal(43);
         assert.isNumber(card);
         assert.isAtLeast(card, 1);
         assert.isAtMost(card, 11);
     });
 
     it('should handle player "hit" action', async function () {
-        // Configure nock to mock the API endpoint
-        nock('https://u0xv62mbgj-u0etdf90is-connect.us0-aws.kaleido.io')
-            .get('/instances/0xd6a231bd45de2c6b1d200c2b5778c810e53cd134/shuffleCards')
-            .query(true)
-            .reply(200, { output: ['1', '2', '3', '4', '10', '11'] }); // Replace with your mock data
-
         await game.hit();
         const player = game.getPlayer();
         assert.isNotEmpty(player.cards);
     });
 
     it('should handle player "stand" action', async function () {
-        // Configure nock to mock the API endpoint
-        nock('https://u0xv62mbgj-u0etdf90is-connect.us0-aws.kaleido.io')
-            .get('/instances/0xd6a231bd45de2c6b1d200c2b5778c810e53cd134/shuffleCards')
-            .query(true)
-            .reply(200, { output: ['1', '2', '3', '4', '10', '11'] }); // Replace with your mock data
-
         await game.stand();
         const dealer = game.getDealer();
         const player = game.getPlayer();
@@ -76,12 +58,6 @@ describe('GameLogic', function () {
     });
 
     it('should handle player "reset" action', async function () {
-        // Configure nock to mock the API endpoint
-        nock('https://u0xv62mbgj-u0etdf90is-connect.us0-aws.kaleido.io')
-            .get('/instances/0xd6a231bd45de2c6b1d200c2b5778c810e53cd134/shuffleCards')
-            .query(true)
-            .reply(200, { output: ['1', '2', '3', '4', '10', '11'] }); // Replace with your mock data
-
         game.restGame()
         const dealerReset = game.getDealer();
         const playerReset = game.getPlayer();
@@ -90,52 +66,27 @@ describe('GameLogic', function () {
     });
 
     it('should handle card deck reduce', async function () {
-        // Configure nock to mock the API endpoint
-        nock('https://u0xv62mbgj-u0etdf90is-connect.us0-aws.kaleido.io')
-            .get('/instances/0xd6a231bd45de2c6b1d200c2b5778c810e53cd134/shuffleCards')
-            .query(true)
-            .reply(200, { output: ['1', '2', '3', '4', '10', '11'] }); // Replace with your mock data
-
         // After hit, player & dealer will have a card. Once rest, deck should be empty
         await game.getNewDeck();
-        assert.equal(game.getPlayDeck().length, 6);
+        assert.equal(game.getPlayDeck().length, 44);
 
         const card = await game.dealCard();
-        assert.equal(game.getPlayDeck().length, 5);
+        assert.equal(game.getPlayDeck().length, 43);
     });
 
     it('should handle new deck shuffle', async function () {
-        // Configure nock to mock the API endpoint
-        nock('https://u0xv62mbgj-u0etdf90is-connect.us0-aws.kaleido.io')
-            .get('/instances/0xd6a231bd45de2c6b1d200c2b5778c810e53cd134/shuffleCards')
-            .query(true)
-            .reply(200, { output: ['1', '2', '3', '4', '10', '11'] }); // Replace with your mock data
-
         await game.getNewDeck();
-        assert.equal(game.getPlayDeck().length, 6);
+        assert.equal(game.getPlayDeck().length, 44);
         assert.notEqual(game.getPlayDeck, [1,2,3,4,10,11]);
     });
 
     it('should handle temp dealer, only show one card in his deck', async function () {
-        // Configure nock to mock the API endpoint
-        nock('https://u0xv62mbgj-u0etdf90is-connect.us0-aws.kaleido.io')
-            .get('/instances/0xd6a231bd45de2c6b1d200c2b5778c810e53cd134/shuffleCards')
-            .query(true)
-            .reply(200, { output: ['1', '2', '3', '4', '10', '11'] }); // Replace with your mock data
-
-
         await game.startGame();
         let dealerCards = game.getTempDealer();
         assert.equal(dealerCards.cards.length, 1);
     });
 
     it('should handle check score, and return game stats object', async function () {
-        // Configure nock to mock the API endpoint
-        nock('https://u0xv62mbgj-u0etdf90is-connect.us0-aws.kaleido.io')
-            .get('/instances/0xd6a231bd45de2c6b1d200c2b5778c810e53cd134/shuffleCards')
-            .query(true)
-            .reply(200, { output: ['1', '2', '3', '4', '10', '11','1', '2', '3', '4', '10', '11'] });
-
         await game.startGame();
         await game.stand();
         let gameStats = game.checkScore();
